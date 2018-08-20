ed021c932877a524a783a565bc5c24153263df3b
Yamabe Naoya <naoya.mail.p@gmail.com>
2018-08-20 14:27:47 +0900

    Add GameManagerの更新 Spaceキーによってお金を増加させる機能を実装

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	MoneyManager moneyManagerScript;
+
+	// Use this for initialization
+	void Start () {
+		moneyManagerScript.ChangeMoney(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+	}
+
+	private void Run(){
+		// Space入力で100円追加
+		if(Input.GetKeyDown("space")){
+			moneyManagerScript.AddMoney(100);
+		}
+	}
+}
