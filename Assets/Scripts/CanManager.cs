0ef903e616d9334fbdac167878d8110ea19bdbf5
Yamabe Naoya <naoya.mail.p@gmail.com>
2018-08-20 21:01:52 +0900

    Add 缶ジュースが購入後召喚される機能を実装

--- Assets/Scripts/CanManager.cs
+++ Assets/Scripts/CanManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CanManager : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,8 @@ public class CanManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector2 pos = transform.position;
+		pos -= new Vector2(0.1f, 0);
+		this.transform.position = pos;
 	}
 }
--- Assets/Scripts/VMManager.cs
+++ Assets/Scripts/VMManager.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class VMManager : MonoBehaviour {
-	MoneyManager moneyManagerScript;
-
+	private MoneyManager moneyManagerScript;
+	[SerializeField] private GameObject can;
+	private GameObject canEntryPoint;
 	// Use this for initialization
 	void Start () {
 		moneyManagerScript = GameObject.Find("GameManager").GetComponent<MoneyManager>();
+		canEntryPoint = GameObject.Find("CanEntryPoint");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// 指定キーの入力で商品を購入
 		if(Input.GetKeyDown(KeyCode.A)){
-			if(moneyManagerScript.GetMoney() > 100) moneyManagerScript.AddMoney(-100);
+			if(moneyManagerScript.GetMoney() >= 100) {
+				moneyManagerScript.AddMoney(-100);
+				Instantiate(can, canEntryPoint.transform.position, Quaternion.identity);
+			}
 		}
 	}
 }
