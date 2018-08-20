4be29d8807a1932eeef5298098e31945d7468f85
Yamabe Naoya <naoya.mail.p@gmail.com>
2018-08-20 13:46:05 +0900

    Add MoneyManager.cs お金の管理スクリプト

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour {
+	private int money;
+
+	// Use this for initialization
+	void Start () {
+		money = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddMoney(int x){
+		money += x;
+	}
+
+	public void ChangeMoney(int x){
+		money = x;
+	}
+
+	public int GetMoney(){
+		return money;
+	}
+}
